d21a267fcc09da6776070b9768a1f90d4ef890cf
Karoly Negyesi <chx1975@gmail.com>
2017-04-30 03:38:37 -0700

    AccountForm::submitForm cleans up, we do not need to.

--- src/EventSubscriber/PasswordChangeEnforcer.php
+++ src/EventSubscriber/PasswordChangeEnforcer.php
@@ -7,7 +7,6 @@ use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Url;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
@@ -29,7 +28,7 @@ class PasswordChangeEnforcer implements EventSubscriberInterface  {
 
   public function __construct(RouteMatchInterface $route_match, AccountInterface $account) {
     // Maybe it is not the best to call methods here but this service will
-    // always run and on either events both are needed.
+    // always run and both are needed.
     $this->routeName = $route_match->getRouteName();
     $this->accountId = $account->id();
   }
@@ -41,36 +40,22 @@ class PasswordChangeEnforcer implements EventSubscriberInterface  {
     $session_key = "pass_reset_$this->accountId";
     if ($this->routeName != 'entity.user.edit_form' && isset($_SESSION[$session_key])) {
       drupal_set_message(t('You must change your password.'));
-      $event->setResponse(new RedirectResponse($this->getUrl($_SESSION[$session_key])));
+      // Copied from \Drupal\user\Controller\UserController::resetPassLogin()
+      $url = Url::fromRoute('entity.user.edit_form',
+        ['user' => $this->accountId],
+        [
+          'query' => ['pass-reset-token' => $_SESSION[$session_key]],
+          'absolute' => TRUE,
+        ]
+      )->toString();
+      $event->setResponse(new RedirectResponse($url));
     }
-  }
-
-  public function cleanup(FilterResponseEvent $event) {
-    if ($event->getResponse() instanceof RedirectResponse && $this->routeName == 'entity.user.edit_form') {
-      unset($_SESSION['pass_reset_' . $this->accountId]);
-    }
-  }
-
-  /**
-   * @param $token
-   *
-   * @return string
-   */
-  protected function getUrl($token) {
-    return Url::fromRoute('entity.user.edit_form',
-      ['user' => $this->accountId],
-      [
-        'query' => ['pass-reset-token' => $token],
-        'absolute' => TRUE,
-      ]
-    )->toString();
   }
 
   /**
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
-    $events[KernelEvents::RESPONSE][] = ['cleanup', 31];
     $events[KernelEvents::REQUEST][] = ['enforcer', 31];
     return $events;
   }
